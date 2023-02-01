382314a90551edc8bf66cf613602daab666180a9
Julien Roy <julien.roy@epitech.eu>
2023-02-01 14:35:56 +0100

    [ADD] Filled the index.php content

--- index.php
+++ index.php
@@ -3,11 +3,167 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>PhotoTuto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="CSS/main.css">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container-fluid min-width-100 min-height-30px borderb-3-black">
+      <h3 class="fw-100 text-align-center margin-top-10">
+        PhotoTuto
+      </h3>
+
+      <center><input type="text" value="Canon 450D" id="inputPassword6" class="text-align-center max-width-25 form-control" aria-describedby="passwordHelpInline"> </center>
+      <br>
+    </div>
+
+    <br>
+
+    <div class="container-fluid borderb-3-black">
+
+      <div class="row">
+
+        <div class="col-sm">
+
+          <center>
+
+            <h3 class="fd-underline fw-300 margin-top-10">Spécifications</h3>          </center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-left-10">
+              Année : 2008 <br>
+              Objectif : 18-55 mm Monture : EF - EF-S <br>
+              ISO : 100-1600. V/Obscuration : 1/4000 Écran : 3'' xxx xxx <br>
+              Wifi : Non <br>
+              Bluetooth : Non Qualité : 12 Mpx <br>
+              NFC : Non <br>
+              USB : Oui <br>
+              Prix : 500$ <br>
+            </h5>
+
+        </div>
+
+        <div class="col-sm">
+
+          <img class="border-1-black img-fluid" src="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2008/pr/09/canon-eos-450d.jpg">
+
+        </div>
+
+        <div class="col-sm">
+
+          <center><h3 class="fd-underline fw-300 margin-top-10">Description</h3></center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-right-10">
+              A l’intérieur, les comparaisons avec le 40D s’accumulent : autofocus sur 9 collimateurs, processeur rapide DIGIC III, apparition de la mesure spot... <br>Parfois l’avantage va même au Canon 450D comme la dotation d’un capteur de 12.2 millions de pixels (contre 10,1 sur le 40D).<br> Le constructeur japonais a toutefois cédé aux sirènes du marché compact et grand public en équipant ce boitier d’un slot SecureDigital.
+
+            </h5>
+
+        </div>
+
+      </div>
+
+      <br>
+
+    </div>
+
+  <!-- Objectifs -->
+
+  <div class="margin-top-20 container-fluid borderb-3-black">
+
+      <div class="row">
+
+        <div class="col-sm">
+
+          <center>
+
+            <h3 class="fd-underline fw-300 margin-top-10">Tamron 70-200</h3>          </center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-left-10">
+              Année :2017 <br>
+              Objectif : 70-200 mm Monture : EF - EF-S Type : Portrait <br>
+              Zoom : Manuel <br>
+              Prix : 89$ <br>
+            </h5>
+            <center>
+            <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Voir les avis</a></center><br>
+
+        </div>
+
+        <div class="col-sm">
+
+          <center>
+          <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Objectifs compatibles</a></center><br>
+          <center>
+          <img class="border-1-black img-fluid" src="https://cdn.lesnumeriques.com/optim/produits/581/14520/tamron-sp-70-200-mm-f-2-8-di-vc-usd_1348151171__450_400.jpg"> <br><br></center>
+          <center>
+          <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Ou l'acheter ?</a></center>
+
+        </div>
+
+        <div class="col-sm">
+
+          <center><h3 class="fd-underline fw-300 margin-top-10">Description</h3></center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-right-10">
+              Le téléobjectif version G2 
+              Le nouveau téléobjectif SP 70-200 mm f/2,8 (Modèle A025) version "G2" réinvente le fameux A009 avec une construction optique renforcée.<br> Un stabilisateur d'image VC amélioré, un AF encore plus rapide et plus précis et une Mise au Point Minimale de 95 cm, pour plus de possibilités. De plus, la compatibilité avec les téléconvertisseurs Tamron x1,4 et x2,0 permet d'étendre la longueur focale.
+
+
+            </h5>
+
+        </div>
+
+      </div>
+
+      <br>
+
+    </div>
+
+
+    <!-- Accesoires -->
+
+  <div class="margin-top-20 container-fluid borderb-3-black">
+
+      <div class="row">
+
+        <div class="col-sm">
+
+          <center>
+
+            <h3 class="fd-underline fw-300 margin-top-10">Trepied Mart</h3>          </center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-left-10">
+              Année : 2017 <BR>
+              Taille : 140 cm <BR>
+              Monture : Universelle<BR> Type : Paysage Déclenchement : Distance<BR> Prix : 59$
+            </h5>
+            <center>
+            <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Voir les avis</a></center><br>
+
+        </div>
+
+        <div class="col-sm">
+
+          <center>
+          <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Accessoires Compatibles</a></center><br>
+          <center>
+          <img class="border-1-black img-fluid min-width-100" src="https://i.ebayimg.com/thumbs/images/g/SCYAAOSwU1Bi20hD/s-l300.jpg"> <br><br></center>
+          <center>
+          <a style=" border: 1px solid black; border-radius: 300px; padding: 5px; background-color: black;" class="text-align-center fd-none fc-white">Ou l'acheter ?</a></center>
+
+        </div>
+
+        <div class="col-sm">
+
+          <center><h3 class="fd-underline fw-300 margin-top-10">Description</h3></center>
+            <h5 class="fw-100 margin-top-10 text-align-left margin-right-10">
+              Le trépied photo MART est un accessoire polyvalent pour les photographes de tous niveaux.<br> Il est conçu pour être stable et durable, avec des jambes en aluminium renforcées et des mécanismes de verrouillage solides.<br> Le MART peut s'étendre jusqu'à une hauteur maximale de 1,8 mètre, offrant ainsi suffisamment de hauteur pour une utilisation confortable.
+
+
+            </h5>
+
+        </div>
+
+      </div>
+
+      <br>
+
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
